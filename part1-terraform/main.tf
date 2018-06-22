335b4f48691e5614183dc12761df9686618ba2ea
Thiago de Faria <thiago@thiagomacbook.local>
2018-06-22 11:44:51 +0200

    terraform basic introduction

--- part1-terraform/main.tf
+++ part1-terraform/main.tf
@@ -7,10 +7,23 @@ provider "aws" {
 # resource "type" "name" {
 # }
 resource "aws_instance" "ibm-example" {
-  ami           = "ami-58d7e821"
+  #ami           = "ami-58d7e821"
+  ami           = "ami-7c491f05"
   instance_type = "t2.micro"
+  key_name      = "ibm-example"
 
   tags {
     Name = "ibm-terraform-example"
+  }
+}
+
+resource "aws_instance" "ibm-example-2" {
+  #ami           = "ami-58d7e821"
+  ami           = "ami-7c491f05"
+  instance_type = "t2.micro"
+  key_name      = "ibm-example"
+
+  tags {
+    Name = "ibm-terraform-example-2"
   }
 }
